339d8bbdee2a364df60567a40fdc87821eaec1a9
thibaut <thibaut07600@gmail.com>
2018-10-18 14:50:31 +0200

    bye bye

--- frontend/jdm-app/src/App.js
+++ frontend/jdm-app/src/App.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <AppBar position="static" color="default">
+            <Toolbar>
+                <Typography variant="h6" color="inherit">
+                    JeuxDeMots
+                </Typography>
+            </Toolbar>
+        </AppBar>
     );
   }
 }
